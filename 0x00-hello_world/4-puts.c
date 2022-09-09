fea6aaa8c2d356de60c31abd4a97d28f35aa2533
joelelegbedeALX <joelelegbedeALX@gmail.com>
2022-09-08 19:29:51 -0700

    Task #4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stio.h>
+/**
+ * main - Entry point
+ * C-low_level: To print a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char puzzle[] = "\"Programming is lik'e building a multilingual puzzle";
+	
+	puts(puzzle)
+
+	return(0)
+
+} 
