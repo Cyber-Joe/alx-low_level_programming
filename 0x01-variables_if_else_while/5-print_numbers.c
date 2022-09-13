c0a99d235a3895aa260b9411b6594fbbdbc791d6
joelelegbedeALX <joelelegbedeALX@gmail.com>
2022-09-12 20:43:58 -0700

    Task #5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; l++)
+	{
+		printf("%d\n", l);
+	}
+	return (0);
+}
