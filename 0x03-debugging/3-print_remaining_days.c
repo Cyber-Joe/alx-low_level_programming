3cef766df827c601e820cead8d2dd1252e3ab74f
joelelegbedeALX <joelelegbedeALX@gmail.com>
2022-09-15 19:12:33 -0700

    Task #3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,6 +6,7 @@
  * @month: month in number
  * @day: day of month
  * @year: year
+ * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
@@ -33,4 +34,3 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
-
