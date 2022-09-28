44c2517758ba49cd548b45adb795c2e4be628e16
Cyber-Joe <elegbedeayomide07@gmail.com>
2022-09-28 10:30:04 -0700

    Task #1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
-/*
- * _print_rev_recursion - prints a string in reverse
+/**
+ * _print_rev_recursion - prints a string in reverse.
  * @s: string
- * Return: nothing
+ * Return: no return.
  */
 void _print_rev_recursion(char *s)
 {
