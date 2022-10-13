6496a9bc4b5ede1c8b3e1cf0f9bf06dd6a1ab11d
Cyber-Joe <elegbedeayomide07@gmail.com>
2022-10-12 18:33:23 -0700

    Task #0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "fuunction_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: function pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
