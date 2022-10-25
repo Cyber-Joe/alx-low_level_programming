1affb747de8e610241337a49ff26e3e972afea46
Cyber-Joe <elegbedeayomide07@gmail.com>
2022-10-24 22:58:05 -0700

    Task #0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - pri nts all the elements of a list.
+ * print_listint - prints all the elements of a list.
  * @h: head of a list.
  *
  *
@@ -9,7 +9,7 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
